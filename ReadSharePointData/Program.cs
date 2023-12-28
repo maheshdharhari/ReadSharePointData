41bb738077615526d6fc6a8709e2cd2fd35b9537
maheshdharhari <mahesh.dharhari@gmail.com>
2023-12-28 17:22:07 +0545

    first commit

---
+++ ReadSharePointData/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web;
+
+namespace ReadSharePointData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the url of specific folder");
+            var url = Console.ReadLine();
+            if (url == null) return;
+            var serverRelativeUrl = string.Empty;
+            if (Uri.TryCreate(url, UriKind.Absolute, out var folderUri))
+            {
+                var query = HttpUtility.ParseQueryString(folderUri.Query).Get("id");
+                serverRelativeUrl = query ?? folderUri.AbsolutePath.Replace("/Forms/AllItems.aspx", string.Empty);
+            }
+
+            ReadSharePoint.ReadSharePointFolders(serverRelativeUrl);
+        }
+    }
+}
---
+++ ReadSharePointData/ReadSharePoint.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Security;
+using Microsoft.SharePoint.Client;
+
+namespace ReadSharePointData
+{
+    public static class ReadSharePoint
+    {
+        private const string SharePointUrl = "https://astedfdfrofdfdfdfidnfdfdepal.sharepoint.com/sites/qa7";
+        private const string UserName = "dfdfdf";
+        private const string Password = "dfddfdfddff";
+        private static readonly SecureString SecurePassword = new SecureString();
+
+        static ReadSharePoint()
+        {
+            foreach (var cc in Password)
+            {
+                SecurePassword.AppendChar(cc);
+            }
+        }
+
+        internal static void ReadSharePointFolders(string url)
+        {
+            using (var spContext = new ClientContext(SharePointUrl))
+            {
+                spContext.Credentials = new SharePointOnlineCredentials(UserName, SecurePassword);
+                var currentWeb = spContext.Web;
+                var folderToGet = currentWeb.GetFolderByServerRelativeUrl(url);
+                spContext.Load(folderToGet, f => f.Exists);
+                try
+                {
+                    spContext.ExecuteQuery();
+                    if (folderToGet.Exists)
+                    {
+                        spContext.Load(folderToGet);
+                        spContext.ExecuteQuery();
+                        Console.WriteLine("Found Folder Name: {0}, Path: {1}, at modified: {2}", folderToGet.Name,
+                            folderToGet.ServerRelativeUrl, folderToGet.TimeLastModified);
+                    }
+                    else
+                    {
+                        // Do something else
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
